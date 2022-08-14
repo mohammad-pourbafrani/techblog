d97e0ec80af2d5fb439ef1f11c04c8229fe2b41b
mohammad-pourbafrani <m.pourbafarani@gmail.com>
2022-08-14 17:25:55 +0430

    add article controller

--- lib/components/api_constant.dart
+++ lib/components/api_constant.dart
@@ -2,4 +2,5 @@ class ApiConstant {
   static const hostDlUrl = "https://techblog.sasansafari.com";
   static const baseUrl = "https://techblog.sasansafari.com/Techblog/api/";
   static const getHomeItemsUrl = baseUrl + "home/?command=index";
+  static const getArticleListUrl = baseUrl + "article/get.php?command=new&";
 }
---
+++ lib/controller/article_controller.dart
@@ -0,0 +1,32 @@
+import 'dart:developer';
+import 'package:get/get.dart';
+import 'package:techblog/components/api_constant.dart';
+import 'package:techblog/models/article_model.dart';
+import 'package:techblog/services/dio_service.dart';
+
+class ArticleContriller extends GetxController {
+  RxList<ArticleModel> articleList = RxList();
+
+  RxBool loading = false.obs;
+
+  @override
+  onInit() {
+    super.onInit();
+    getHomeItem();
+  }
+
+  getHomeItem() async {
+    loading.value = true;
+    // ignore: todo
+    //TODO: get user id from getStorage ApiConstant.getArticleListUrl + userid
+    var response = await DioService().getMethod(ApiConstant.getArticleListUrl);
+    if (response.statusCode == 200) {
+      response.data.forEach((element) {
+        articleList.add(ArticleModel.fromJson(element));
+      });
+
+      loading.value = false;
+    }
+    log(articleList.toString());
+  }
+}
